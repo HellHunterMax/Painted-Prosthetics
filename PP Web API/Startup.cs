99ac4d702f8a3e66c6c29d801c547bec7da46fa4
Max <hellhunter15@hotmail.com>
2020-10-10 06:06:24 +0200

    initial

--- PP Web API/Startup.cs
+++ PP Web API/Startup.cs
@@ -26,6 +26,7 @@ namespace PP.Web.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
