faa363c202a610009e245369c8cece7abbfd3ac3
Max <hellhunter15@hotmail.com>
2020-10-18 05:22:32 +0200

    added connectionstring; added context to services;

--- PP Web API/Startup.cs
+++ PP Web API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,9 @@ namespace PP.Web.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GalleryContext>(opt =>
+               opt.UseSqlServer(Configuration.GetConnectionString("GalleryConnection")));
+
             services.AddControllers();
 
             services.AddScoped<IImageRepository, MockImageRepository>();
