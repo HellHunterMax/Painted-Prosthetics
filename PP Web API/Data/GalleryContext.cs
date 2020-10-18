bd9e580ff9c7be38b80299ac1bc025802e088653
Max <hellhunter15@hotmail.com>
2020-10-18 04:56:39 +0200

    created GalleryContext for Db;

---
+++ PP Web API/Data/GalleryContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PP.Web.API.Model;
+
+namespace PP.Web.API.Data
+{
+    public class GalleryContext : DbContext
+    {
+        public GalleryContext(DbContextOptions<GalleryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+    }
+}
