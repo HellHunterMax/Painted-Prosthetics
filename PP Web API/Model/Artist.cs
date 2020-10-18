8915ab611fec82ece93a4924c7dabfe089eed929
Max <hellhunter15@hotmail.com>
2020-10-18 06:12:35 +0200

    added Data Annotations to the Models;

--- PP Web API/Model/Artist.cs
+++ PP Web API/Model/Artist.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PP.Web.API.Model
 {
     public class Artist
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
-        public List<Image> Images { get; set; }
+
+
+        public ICollection<Image> Images { get; set; }
+
+
         public string Bio { get; set; }
+
+
         public string Website { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
--- PP Web API/Model/Image.cs
+++ PP Web API/Model/Image.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PP.Web.API.Model
 {
     public class Image
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public Artist Artist { get; set; }
+
+        [Required]
         public DateTime AddDate { get; set; }
+
         public int Likes { get; set; }
+
+        [Required]
         public string Uri { get; set; }
     }
 }
