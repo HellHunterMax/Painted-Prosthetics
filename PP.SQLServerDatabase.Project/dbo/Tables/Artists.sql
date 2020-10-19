e4766bffdc9ef5401574cd637ea4e85c1a61f197
Max <hellhunter15@hotmail.com>
2020-10-19 05:07:45 +0200

    Added Database Project;

---
+++ PP.SQLServerDatabase.Project/dbo/Tables/Artists.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Artists] (
+    [ArtistId] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (MAX) NOT NULL,
+    [Bio]      NVARCHAR (MAX) NULL,
+    [Website]  NVARCHAR (MAX) NULL,
+    [Email]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Artists] PRIMARY KEY CLUSTERED ([ArtistId] ASC)
+);
+
---
+++ PP.SQLServerDatabase.Project/dbo/Tables/Images.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Images] (
+    [ImageId]  INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (MAX) NOT NULL,
+    [ArtistId] INT            NOT NULL,
+    [AddDate]  DATETIME2 (7)  NOT NULL,
+    [Likes]    INT            NOT NULL,
+    [Uri]      NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Images] PRIMARY KEY CLUSTERED ([ImageId] ASC),
+    CONSTRAINT [FK_Images_Artists_ArtistId] FOREIGN KEY ([ArtistId]) REFERENCES [dbo].[Artists] ([ArtistId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Images_ArtistId]
+    ON [dbo].[Images]([ArtistId] ASC);
+
