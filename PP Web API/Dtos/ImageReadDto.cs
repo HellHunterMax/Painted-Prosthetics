435d8ab06ae0ba48a9417958b5c236a828cae0a6
Max <hellhunter15@hotmail.com>
2020-10-20 04:15:24 +0200

    Created ImageReadDto;

---
+++ PP Web API/Dtos/ImageReadDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PP.Web.API.Model;
+
+namespace PP.Web.API.Dtos
+{
+    public class ImageReadDto
+    {
+        public int ImageId { get; set; }
+        
+        public string Name { get; set; }
+        
+        public int ArtistId { get; set; }
+
+        public Artist Artist { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        public int Likes { get; set; }
+
+        public string Uri { get; set; }
+    }
+}
