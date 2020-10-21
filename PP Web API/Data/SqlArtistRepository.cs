3691c1f617ef8eef8c2b2da8f6c8f8cf8aa7fda3
Max <hellhunter15@hotmail.com>
2020-10-22 01:41:18 +0200

    added Artist Repo;

---
+++ PP Web API/Data/SqlArtistRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PP.Web.API.Model;
+
+namespace PP.Web.API.Data
+{
+    public class SqlArtistRepository : IArtistRepository
+    {
+        private readonly GalleryContext _context;
+
+        public SqlArtistRepository(GalleryContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Artist> GetAllArtists()
+        {
+            return _context.Artists.ToList();
+        }
+
+        public Artist GetArtist(int id)
+        {
+            return _context.Artists.FirstOrDefault(x => x.ArtistId == id);
+        }
+    }
+}
