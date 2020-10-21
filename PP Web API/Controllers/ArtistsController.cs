e11708547cb3e849c7f5b46f688659416b8ac22d
Max <hellhunter15@hotmail.com>
2020-10-22 01:56:50 +0200

    started ArtistController;

---
+++ PP Web API/Controllers/ArtistsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PP.Web.API.Data;
+
+namespace PP.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private readonly IArtistRepository _artistRepository;
+        private readonly IMapper _mapper;
+
+        public ArtistsController(IArtistRepository artistRepository, Mapper mapper)
+        {
+            _artistRepository = artistRepository;
+            _mapper = mapper;
+        }
+    }
+}
